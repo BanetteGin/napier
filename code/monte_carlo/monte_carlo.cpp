c39f03d574b1ae4baa75e17154cfdc0b874f1379
Shuya Nakagawa <BanetteGin@gmail.com>
2023-04-29 08:26:55 +0900

    change

--- code/monte_carlo/monte_carlo.cpp
+++ code/monte_carlo/monte_carlo.cpp
@@ -8,35 +8,7 @@ namespace mpi = boost::multiprecision;
 using Bint = mpi::cpp_int;
 using Real = mpi::number<mpi::cpp_dec_float<1024>>;
 
-
-#define rep(i, n) for(ll i = 0, n_f = (ll)(n); i < n_f; ++i)
-#define all(a) (a).begin(),(a).end()
-#define pb(a) push_back(a)
-#define eb(a) emplace_back(a)
-#define sp fixed<<setprecision(20)
-#define mpa(a,b) make_pair(a,b)
-
 typedef long long ll;
-typedef __int128_t int128;
-typedef double dou;
-typedef long double ld;
-typedef vector<double> vdou;
-typedef vector<string> vstr;
-typedef vector<bool> vbol;
-typedef vector<vector<bool>> vvbol;
-typedef vector<ll> vll;
-typedef vector<vector<ll>> vvll;
-
-const ll dx[] = {-1,0,1,0};
-const ll dy[] = {0,1,0,-1};
-const ll dx2[] = {-1,-1,0,1,1,1,0,-1};
-const ll dy2[] = {0,1,1,1,0,-1,-1,-1};
-const ll mod99ll = 998244353LL;
-const ll mod107ll = 1000000007LL;
-const ll INFL = numeric_limits<ll>::max();
-
-template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 
 int main(void){
     const ll digit=10;
